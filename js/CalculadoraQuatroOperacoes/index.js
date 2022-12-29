319778cb195166f5f76396dd82f22bdd51ebc103
Magno <magno.alefb@gmail.com>
2022-12-29 18:38:50 -0300

    iniciando js

--- js/CalculadoraQuatroOperacoes/index.js
+++ js/CalculadoraQuatroOperacoes/index.js
@@ -1,5 +1,13 @@
 let num1 = window.prompt('Digite um numero:')
 let num2 = window.prompt('Digite outro numero:')
+alert(
+    ` Temos como resultado dessas operações os valores abaixo: \n
+    ${num1} + ${num2} = ${parseInt(num1) + parseInt(num2)} \n
+     ${num1} - ${num2} = ${num1-num2} \n
+     ${num1} * ${num2} = ${num1*num2} \n
+     ${num1} / ${num2} = ${num1/num2} \n
+    ` 
+)
 console.log(parseInt(num1)+parseInt(num2))
 console.log(num1-num2)
 console.log(num1*num2)
--- js/PrimeiroExercicio/index.js
+++ js/PrimeiroExercicio/index.js
@@ -1,8 +1,16 @@
-nome = window.prompt('Insira seu primeiro nome:')
-sobrenome = window.prompt('Insira seu sobrenome:')
-campoEstudo = window.prompt('Insira seu campo de estudo:')
-anoNascimento = window.prompt('Insira seu ano de nascimento:')
-idade = 2022 - anoNascimento
-
+ var nome = window.prompt('Insira seu primeiro nome:')
+ var sobrenome = window.prompt('Insira seu sobrenome:')
+ var campoEstudo = window.prompt('Insira seu campo de estudo:')
+ var anoNascimento = window.prompt('Insira seu ano de nascimento:')
+const dataAtual = new Date()
+const anoAtual = dataAtual.getFullYear()
+const idade = anoAtual - anoNascimento
 // console.log('Seu nome completo e ',nome , '', sobrenome ,'Seu campo de estudo é ', campoEstudo ,'Sua idade e ',idade)
+alert(
+    "Recruta cadastrado com sucesso:\n " +
+    "\nnome completo: " + nome + ' ' + sobrenome +
+    "\nCampo de estudo: " + campoEstudo +
+    "\nidade: " + idade
+)
 console.log(`Seu nome completo é ${nome} ${sobrenome}, Seu campo de estudo é ${campoEstudo} e sua idade é ${idade}`)
+// \n quebra de linha 
