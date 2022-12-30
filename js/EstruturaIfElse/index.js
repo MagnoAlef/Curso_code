18d075a0def9320e3ecbd7e22513e6ecf064e33f
Magno <magno.alefb@gmail.com>
2022-12-30 16:28:45 -0300

    comecando osestudos

---
+++ js/EstruturaIfElse/index.js
@@ -0,0 +1,16 @@
+// const idade = window.prompt('Qual a sua idade ?')
+
+// if(idade >= 18){
+//     alert('Voce e maior  de idade')
+// }else if(idade > 5){
+//     alert('Voce e menor de idade')
+// }else if(idade < 4){
+//     alert('Voce e criança')
+// }
+// else{
+//     alert('...')
+// }
+//Operador Ternario
+//const resultado = condicao ? resultado_verdadeiro : resultado_falso 
+const resultado = (5 === 5) ? "Verdadeiro" : "Falso"
+console.log(resultado)
