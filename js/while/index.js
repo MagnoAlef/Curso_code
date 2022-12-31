e8d1cf749a2a41cb902800aa6a58fbce933c1710
Magno <magno.alefb@gmail.com>
2022-12-31 17:15:09 -0300

    estruturas de repetição

---
+++ js/while/index.js
@@ -0,0 +1,7 @@
+let velocidade = 100
+
+while(velocidade > 0 ){
+    alert(`A velocidade atual e ${velocidade}`)
+    velocidade = velocidade - 20
+    alert(`Estamos diminuindo 20Km/h a velocidade atual é ${velocidade}`)
+}
