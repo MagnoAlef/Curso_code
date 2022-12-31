8790ff038cb601fef8d9d8f6b6200ce2b8a055b1
Magno <magno.alefb@gmail.com>
2022-12-30 22:13:22 -0300

    exercicio switch teste

--- js/ConversorMedidas/index.js
+++ js/ConversorMedidas/index.js
@@ -1,16 +1,27 @@
 let medidas = prompt(
   "Digite um valor em metros, para ser convertida nas seguintes medidas:"
 );
+// let opcoes = prompt(
+//   "Digite para qual unidade de medida vc queira converter:\n milimetro(mm)\n centimetro(cm)\n decimetro(dm)\n decametro(dam)\n hectometro(hm)\n quilometro(km)\n"
+// );
+
 let opcoes = prompt(
-  "Digite para qual unidade de medida vc queira converter:\n milimetro(mm)\n centimetro(cm)\n decimetro(dm)\n decametro(dam)\n hectometro(hm)\n quilometro(km)\n"
-);
+       "Digite para qual unidade de medida vc queira converter:"+
+       "\n milimetro(mm)"+
+       "\n centimetro(cm)"+
+       "\n decimetro(dm)"+
+       "\n decametro(dam)"+
+       "\n hectometro(hm)"+
+       "\n quilometro(km)"+
+       "\n"
+     );
 
 switch (opcoes) {
   case "mm":
     alert(`${medidas*1000}`);
     break;
   case "cm":
-    alert(`${medidas*1000}`);
+    alert(`${medidas*100}`);
     break;
   case "dm":
     alert(`${medidas*10}`);
