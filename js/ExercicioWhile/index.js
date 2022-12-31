b7c3003133207d16d2822aa920cacafcac2187d1
Magno <magno.alefb@gmail.com>
2022-12-31 18:05:00 -0300

    estruturas while

--- js/ExercicioWhile/index.js
+++ js/ExercicioWhile/index.js
@@ -1,13 +1,12 @@
- const turista  = prompt("Qual e seu nome turista?")
- contagem = 0
- cidades  = ''
-resposta = prompt('Voce visitou alguma cidade ?  [Sim] / [Não] ')
-while (resposta === 'Sim'){
-   cidade = prompt('Qual o nome das cidades visitadas?')
-   //cidades+= "-" + cidade + "\n"
-   cidades = cidades + "." + cidade + "\n"
-   contagem++
-   resposta = prompt('Voce visitou alguma cidade ?  [Sim] / [Não] ')
+const nome = prompt('Qual seu nome:')
+contagem = 0 
+cidades = ''
+continuar = prompt('Voce visitou  alguma cidade? [Sim]/[Não]')
+while(continuar === 'Sim'){
+    cidade = prompt('Qual cidade voce visitou ?')
+    contagem ++
+    cidades = cidades + "-" + cidade  + "\n"
+    continuar = prompt('Voce visitou mais alguma cidade? [Sim]/[Não]')
 }
-
-alert(`o nome do turista é ${turista}:\n Ele visitou um total de ${contagem}  cidades as cidades visitas foram:\n ${cidades} `)
+alert(`O nome do turista é ${nome} \n 
+Ele visistou o total de ${contagem} cidades e as cidades visitadas foram: \n ${cidades}`)
