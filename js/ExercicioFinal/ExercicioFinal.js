8d155a8c8d019b2f0e8b980e56f38ea77f5e2c39
Magno <magno.alefb@gmail.com>
2023-01-31 21:26:08 -0300

    Exercicio Final

---
+++ js/ExercicioFinal/ExercicioFinal.js
@@ -0,0 +1,23 @@
+/**
+ * Escreva um programa em js que simule um sitema de vagas de emprego onde e possivel gerenciar as
+ * vagas e adicionar candidatos a vagas ele deve atender aos seguintes requisitos.
+ * Ter um menu onde e possivel escolher entre as diferentes funcionalidades do sistema
+ * -> Listar vagas disponiveis
+ * -> Criar uma nova vaga
+ * -> Visualizar uma vaga
+ * -> Inscrever um canditado em uma vaga 
+ * -> excluir vaga
+ * -> Sair
+ * A opcao de listar as vagas deve mostrar o indice o nome e a quantidade de candidatos inscritos de todas as vagas.
+ * A opçao de criar uma nova vaga deve pedir um nome para a vaga, uma descrição e uma data limite e tambem
+ * dece pedir que o usuario confirme as informações antes de salva-la.
+ * A opção de visualizar uma vaga deve pedir o indice da vaga e mostrar todas as informações dela -> 
+ * indice nome descriçao data limite quantidade de candidatos e o nome dos candidatos
+ * A opção de inscrever um canditado em uma vaga deve pedir o nome do candidato o indice da vaga e entao uma 
+ * confirmação exibindo as informações da vaga antes de salvar o candidato na vaga.
+ * A opção de excluir uma vaga deve pedir o indice da vaga, mostrar suas informações e pedir que o usuario confirme
+ * a exclusão da vaga antes de realmente exclui-la
+ * 
+ *    Este e o exercicio de revisao do modulo, entao aproveite para utilizar todos os recursos vistos ate agora sempre que 
+ * possivel comos os obejtos arrays e funcoes. 
+ */
