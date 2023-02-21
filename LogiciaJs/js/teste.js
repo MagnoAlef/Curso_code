7bf565538342c7224aa3ca7bccaa6de049636ab8
Magno <magno.alefb@gmail.com>
2023-02-21 14:58:49 -0300

    troca de pasta

---
+++ LogiciaJs/js/teste.js
@@ -0,0 +1,14 @@
+let alunos = [
+    {nome: 'jose' , idade : 18},
+    {nome: 'joao' , idade : 16},
+    {nome: 'maria' , idade : 21},
+    {candidato:['teste']}
+]
+
+let cadidato = []
+//alunos.push(cadidato)
+ let teste = 'magno'
+
+//  alunos[3].push(teste)
+  console.log(alunos)
+ //console.log(alunos[3][0])
