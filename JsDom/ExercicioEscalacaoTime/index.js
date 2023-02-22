facdf78972108c84a1949c178bca403c122cdab1
Magno <magno.alefb@gmail.com>
2023-02-22 14:52:29 -0300

    exercicio usando o Dom

---
+++ JsDom/ExercicioEscalacaoTime/index.js
@@ -0,0 +1,45 @@
+/**
+ * Construa uma pagina web que permita montar a escalação de um time de jogadores.
+ * Nela sera possivel.
+ * Escalar um jogador
+ * -> informar a posição do jogador a ser escalado para o time
+ * ->informa o nome do jogador 
+ * ->informar o numero da camisa do jogador
+ * Ter um botao Escalar que pergunta ao usuario se ele deseja confirmar a escalação 
+ * daquele jogador e entao insere as informações em uma lista na pagina 
+ * Apos o jogador ser escalado os campos de texto devem ser limpos.
+ * Remover um jogador
+ * ->informar o numero da camisa do jogador
+ * -> ter um bota Remover que pergunta ao usuario se ele deseja confirmar a remocao daquele
+ * jogador e entao excluir ele da lista da pagina
+ * Apos o jogador ser removido o campo de texto deve ser limpo
+ * Dica:lembrando que e possivel acessar o texto de um input atraves da propriedade value.
+ */
+function addNewPlayer(){
+    const position = document.getElementById('position').value //Pegando o valor do input
+    const name = document.getElementById('name').value
+    const number = document.getElementById('number').value
+    console.log({position,name,number})
+    const confirmacao = confirm("Escalar " + name + " como " + position + '?')
+    if(confirmacao){ // sempre se atentar com nomes reservados
+        const teamList= document.getElementById('teamList') //Pegando o elemento da ul do html
+        const playerItem = document.createElement('li') //criando elemento li no html
+        playerItem.id = 'player-' + number // id que sera criado apartir dessa propriedade
+        playerItem.innerText = position + ': ' + name + "(" + number + ")" // nome que aparecera na pagina
+        teamList.appendChild(playerItem) //adicionado na ul a li 
+
+        document.getElementById('position').value = '' // Limpando input
+        document.getElementById('name').value = ''
+        document.getElementById('number').value = ''
+    }
+}
+ function removePlayer(){
+     const number = document.getElementById('numberToRemove').value
+     const playerToRemove = document.getElementById('player-' + number) // id criado na li da funcao acima
+ const confirmation = confirm("Remover o jogador " + playerToRemove.innerText + '?')
+ if(confirmation){
+    document.getElementById('teamList').removeChild(playerToRemove) //selecionar a ul toda e remove todos os elementos
+     // playerToRemove.remove() remocao direta
+     document.getElementById('numberToRemove').value = ''
+ }
+    }
