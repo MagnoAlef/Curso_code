f22f3b120489144b4200c30436be06dd3de14361
Magno <magno.alefb@gmail.com>
2023-03-06 21:27:26 -0300

    criando elementos com dom

--- JsDom/CriandoNovosElementosNoDom/index.js
+++ JsDom/CriandoNovosElementosNoDom/index.js
@@ -1,14 +1,15 @@
 function addInput() {
-    const ul = document.getElementById('inputs')
-  
-    const newLi = document.createElement('li')
-    newLi.className = 'list-item'
-    newLi.innerText = 'Novo input: '
-  
-    const newInput = document.createElement('input')
-    newInput.type = 'text'
-    newInput.name = 'input'
-  
-    newLi.appendChild(newInput)
-    ul.appendChild(newLi)
+const ul = document.getElementById('inputs')
+
+const newLi = document.createElement('li')
+newLi.className = 'item-classe'
+newLi.innerText= 'Novo input:'
+
+const novoInput = document.createElement('input')
+novoInput.id = 'id-novo-input'
+novoInput.type = 'text'
+
+newLi.appendChild(novoInput)
+ul.appendChild(newLi)
+
   }
