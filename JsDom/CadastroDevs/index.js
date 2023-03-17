2ab65709fdb16fa4a7d9df7b65ce896fc805a535
Magno <magno.alefb@gmail.com>
2023-03-16 22:29:14 -0300

    cadastro de devs

---
+++ JsDom/CadastroDevs/index.js
@@ -0,0 +1,15 @@
+/**
+ * Construa uma pagina web que permita cadastrar deselvolvedores e suas tecnologias
+ * e tempo de experiencia
+ * A pagina deve ter um input para o nome completo do desenvolvedor e um botao para adicionar
+ * uma nova tecnologia
+ * Ao clicar no botao um evento deve ser adicionado criando no formulario uma nova linha de input contendo
+ * um campo de texto para o nome da teconologia 
+ * um grupo de botoes tipo radio com as opcoes de tempo de experiencia (por exemplo
+ *  0-2 3-4 5ou+)
+ * um botao de remover que deve excluir essa linha 
+ * O formulario tambem deve ter um botao de cadastrar que salva as informações do dev em um array e limpa o formulario para
+ * testar o funcionamento voce pode utilizar um simples console.log()
+ * Para esse exercicio voce deve utilizar apenas eventos adicionados via js e a 
+ * pagina deve funcionar sem acionar um recarregamento
+ */
