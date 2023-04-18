ba5abfd275803f2320b0ffdeb71fda5289492049
Magno <magno.alefb@gmail.com>
2023-04-17 22:29:59 -0300

    exercicio

---
+++ RevisaoLogicaJs/CadastroUsuario/index.js
@@ -0,0 +1,11 @@
+/**
+ * Cadastros de recrutas
+ * Escreva um programa em js qaue permita salvar informações de um recruta.
+ * As informações a serem salvas são:
+ * O primeiro nome 
+ * O sobrenome
+ * O campo de estudo 
+ * O ano de nascimento
+ * Depois o programa deve ser exibir o nome completo do recruta seu campo de 
+ * estudo e sua idade (apenas baseada no ano de nascimento)
+ */
---
+++ RevisaoLogicaJs/Calculadora/index.js
@@ -0,0 +1,5 @@
+/**
+Escreva um programa em js que permitar inserir dois valores numericos e entao calcule
+o resultado das quatro operações basicas(soma,subtração,multiplicação e divisão)
+Apos calcular os resultados o programa deve exibi-los na tela
+ */
