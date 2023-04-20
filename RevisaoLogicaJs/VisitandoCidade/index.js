88825133e4394dca6cf1196f36e6a61e2994b709
Magno <magno.alefb@gmail.com>
2023-04-19 22:02:32 -0300

    Exercicio Visitando cidades

---
+++ RevisaoLogicaJs/VisitandoCidade/index.js
@@ -0,0 +1,7 @@
+/**
+ * Escreva um programa em js que peça o nome de um turista e então 
+ * pergunte se ele ja visitou alguma cidade.Caso a resposta seja sim, pergunte o 
+ * nome da cidade e o armazene em uma variavel, e então continue perguntando se o turista 
+ * visitou alguma outra cidade até que a resposta seja não. no fim,o
+ * programa deve exibir o nome do turista , quantas cidades ele visitou
+ */
