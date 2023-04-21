c3425e4935fd532f7c047d7b547601daad3ac0e5
Magno <magno.alefb@gmail.com>
2023-04-20 21:19:18 -0300

    exercio control financeiro e menu interativo

---
+++ RevisaoLogicaJs/ControleFinanceiro/index.js
@@ -0,0 +1,8 @@
+/**
+ * Escreva um programa em js que comece perguntando pela quantidade inicial de dinheiro
+ * disponivel e então mostre na tela essa quantidade juntamente com as opções de adicionar e 
+ * remover dinheiro e uma opção de Sair.Ao clicar na opção de adicionar dinheiro o programa deve
+ * perguntar pela quantidade a ser adicionada , somar esse valor com a quantidade inicial e então 
+ * mostrar novamente o menu com a quantidade de dinheiro e as opções.A opção de remover 
+ * dinheiro deve fazer o mesmo, porem subtraindo o valor. A opção de sair deve encerrar o programa.
+ */
---
+++ RevisaoLogicaJs/MenuInterativo/index.js
@@ -0,0 +1,9 @@
+/**
+ * Escreva um programa em js que simule um menu interativo com 5 opções diferentes.O
+ * programa devera iniciar mostrando as 5 opções disponiveis as quatro primeiras não precisam de 
+ * nenhuma funcionalidade especifica a ultima opção deve ser a opção "Encerrar".Ao clicar em alguma
+ * das quatro primeiras opções uma mensagem deve ser exibida na tela indicando qual foi a opção
+ * escolhida e depois o programa deve exibir novamente o menu com as 5 opções. Isso  deve continuar 
+ * acontecenco ate que o usuario escolha a opção"Encerrar".Ao escolher uma mensagem dizendo
+ * que o sistema esta sendo encerrado deve ser exibida na tela e então o programa e finalizado.
+ *  */
