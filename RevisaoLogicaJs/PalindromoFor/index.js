f4728ef06e51d371109e5669d55ccc95895d469d
Magno <magno.alefb@gmail.com>
2023-04-26 23:04:24 -0300

    Resolucao exercicio palindromo

--- RevisaoLogicaJs/PalindromoFor/index.js
+++ RevisaoLogicaJs/PalindromoFor/index.js
@@ -5,3 +5,41 @@
  * dizendo se aquela palavra e um palindromo.Caso não seja um palindromo, o programa deve
  *  mostrar a palavra da esquerda para a direita e da direita para a esquerda
  */
+
+let palavra = prompt('Digite uma palavra para analisar se ela e um palindromo:')
+let palavraDireita = ''
+let palavraEsquerda = ''
+for ( let i = 0 ; i < palavra.length ; i++){
+    palavraDireita+= palavra[i]
+}
+
+for ( let i = palavra.length -1 ; i  >= 0  ; i--){
+    palavraEsquerda += palavra[i]
+}
+
+if(palavraDireita === palavraEsquerda){
+    alert(`As palavras ${palavraDireita} e ${palavraEsquerda} são iguais:`)
+}else{
+
+    alert(palavraDireita)
+    alert(palavraEsquerda)
+    alert('Não são palindromos:')
+}
+
+/**
+ * Professor
+ * 
+ * const palavra = prompt("Informe uma palavra:")
+let palavraInvertida = ""
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+  palavraInvertida += palavra[i]
+}
+
+if (palavra === palavraInvertida) {
+  alert(palavra + " é um palíndromo!")
+} else {
+  alert(palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavraInvertida)
+}
+ * 
+ */
--- RevisaoLogicaJs/TabuadaFor/index.js
+++ RevisaoLogicaJs/TabuadaFor/index.js
@@ -12,3 +12,14 @@ for (let i = 1 ; i <= 20 ;i++ ){
     number = number + '\n' + i * numero + '-'
 }
 alert(number)
+/**
+ * Professor
+const numero = parseFloat(prompt("De qual número você deseja calcular a tabuada?"))
+let resultado = ""
+
+for (let fator = 1; fator <= 20; fator++) {
+  resultado += " -> " + numero + " x " + fator + " = " + (numero * fator) + "\n"
+}
+
+alert("Resultado da Tabuada de " + numero + ":\n\n" + resultado)
+ */
