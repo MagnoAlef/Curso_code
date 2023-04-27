ea7da693395903cd1301c497446c463ab511218b
Magno <magno.alefb@gmail.com>
2023-04-26 22:24:08 -0300

    exercicio for

---
+++ RevisaoLogicaJs/PalindromoFor/index.js
@@ -0,0 +1,7 @@
+/**
+ * Escreva um programa em js que seja capaz de identificar se uma palavra e um palindromo.Um
+ * palinndromo e uma palavra que lida de tras para frente possuo as mesmas letras na mesma ordem.O
+ * programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem 
+ * dizendo se aquela palavra e um palindromo.Caso não seja um palindromo, o programa deve
+ *  mostrar a palavra da esquerda para a direita e da direita para a esquerda
+ */
---
+++ RevisaoLogicaJs/TabuadaFor/index.js
@@ -0,0 +1,7 @@
+/**
+ * Escreva um programa em js que seja capaz de calcular a tabuada de um determinado numero
+ * (1 a 20).O programa deve começar pedindo o número a ser usado nos calculos e então salvar 
+ * todos os resultados de multipliação de 1 a 20 usando o numero informado dentro de uma variavel 
+ * do tipo string.Depois o programa deve exibir esses resultados e finalizar.
+ * 
+ */
