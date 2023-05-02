5efd2e2d3e1a17bfe1533e6ee75502f079fc51cf
Magno <magno.alefb@gmail.com>
2023-05-02 20:15:19 -0300

    teste js funcoes

--- RevisaoLogicaJs/teste/teste.js
+++ RevisaoLogicaJs/teste/teste.js
@@ -11,3 +11,31 @@ console.log(x)
 minhaFuncao(meusCarros)
 y=meuCarro.make
 console.log(y)
+//Function fatorial
+// var fatorial = function fac(n) {
+//     return n<2 ? 1 : n*fac(n-1)
+// }
+// console.log(fatorial(4))
+
+function fatorial(n){
+    return n < 2 ? 1 : n*fatorial(n-1)
+}
+console.log(fatorial(5))
+
+function map(f,a) {
+    console.log('a',a)
+    console.log('a',f)
+    var result = []; // Cria um novo Array
+    var i;
+    for (i = 0; i != a.length; i++)
+      result[i] = f(a[i]);
+      console.log('testes',result)
+    return result;
+  }
+   map(function(x) 
+   {
+     console.log(x)
+     return x * x * x
+    }
+     ,
+      [0, 1, 2, 5, 10]);
