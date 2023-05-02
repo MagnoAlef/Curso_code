f392c64daf082565b73c2bd62611c60f55936de5
Magno <magno.alefb@gmail.com>
2023-05-02 19:52:16 -0300

    teste js

---
+++ RevisaoLogicaJs/teste/teste.js
@@ -0,0 +1,13 @@
+function minhaFuncao(objeto){
+    console.log('testes',objeto[0])
+}
+
+var meuCarro = {make:'Honda',model:'Accord',year:1998}
+var meusCarros = ['toyota','honda','fusca']
+var x,y;
+x = meuCarro.make
+console.log(x)
+
+minhaFuncao(meusCarros)
+y=meuCarro.make
+console.log(y)
