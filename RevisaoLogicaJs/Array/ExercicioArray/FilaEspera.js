b25108e357537d8fedca57ce4bddc1af402d0079
Magno <magno.alefb@gmail.com>
2023-05-05 21:14:00 -0300

    exercicio js

---
+++ RevisaoLogicaJs/Array/ExercicioArray/FilaEspera.js
@@ -0,0 +1,9 @@
+/**
+ * Escreva um programa em js para simular um fila de espera em um consultorio medico.O
+ * programa deve iniciar mostrando na tela um menu interativo contendo a lista de todos os pacientes
+ * esperando em ordem mostrando sua posição ao lado do nome (ex: 1 Mateus 2 marcos,etc) O 
+ * menu tambem deve permitir escolher entre as opções de "Novo Paciente" para adicionar um novo
+ * paciente ao fim da fila(pedindo o nome do paciente) 'consultar paciente' que retira o primeiro
+ * paciente da fila e mostra mna tela o nome do paciente consultado e Sair.O programa so deve ser
+ * encerrado ao escolher a opção de Sair , caso contrario deve voltar ao menu. 
+ */
---
+++ RevisaoLogicaJs/Array/ExercicioArray/JogoCartas.js
@@ -0,0 +1,8 @@
+/**
+ * Escreva um programa em js para simular um baralho de cartas . o programa deve iniciar mostrando
+ * na tela um menu interativo contendo as cartas que estão atualmente no baralho e as opções de Adicionar
+ * uma carta Puxar uma carta e Sair. Ao escolher Adicionar uma carta o programa deve perguntar o 
+ * nome da carta e adiciona-la no topo do baralho. Ao escolher Puxar uma carta o programa deve retirar a 
+ * carta do topo do baralho e mostrar na tela o nome da carta puxada. O programa so deve ser encerrado ao 
+ * escolher a opção de Sair, caso contrario deve voltar ao menu.
+ */
