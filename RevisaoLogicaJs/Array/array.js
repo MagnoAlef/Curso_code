dfeb00563234092efbc03f417425cd2ee6a14ba5
Magno <magno.alefb@gmail.com>
2023-05-05 21:01:15 -0300

    continuando com array

--- RevisaoLogicaJs/Array/array.js
+++ RevisaoLogicaJs/Array/array.js
@@ -29,3 +29,17 @@ console.log(indice)
 //Caso usar numeros negativos o -1 e o ultimo numero, caso queira pegar todos os numeros de traz para frente e so colocar o -1
 const sliceTeste = arr.slice(1,5) // Não pega o ultimo indice
 console.log(sliceTeste)
+//Concat -> concatenar
+const sociedade  = arr.concat('teste','testes1')
+console.log(sociedade)
+
+//Substituir Elementos
+//splice -> remove um grupo de elementos e subtituir por outro
+const a =  arr.splice(4,1 , 'superTeste') //Passa o indice de onde comecar a remover, segundo parametro quantos elementos deseja excluir, terceiro parametro o que e para substituir  
+console.log(arr)
+console.log(a) // Vai trazer o elemento removido
+
+//Iterar sobre os elementos do array
+for (let indice = 0 ; indice < arr.length; indice++){
+    console.log(arr[indice])
+}
