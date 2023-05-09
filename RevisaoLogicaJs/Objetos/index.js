6ce56bd4631269c355e0b46c32a79eb04c48644d
Magno <magno.alefb@gmail.com>
2023-05-08 21:18:20 -0300

    exercico  objetos

---
+++ RevisaoLogicaJs/Objetos/index.js
@@ -0,0 +1,12 @@
+/**
+ * Escreva um programa em js que funcione como um cadastro de imoveis e atenda aos seguintes requisitos.
+ * Dete ter um menu interativo que sempre e exibido ate que o usuario escolha Sair.
+ * O menu interativo deve mostrar no topo a quantidade de imoveis cadastrados.
+ * O menu deve ter a opção de Salvar um imovel.
+ *  Para salvar um novo imovel o programa deve pedir as seguintes informações :
+ * -> Nome do proprietario
+ * -> Quantidade de quartos
+ * -> Quantidade de banheiros
+ * -> Se possui garagem
+ * O menu tambem deve ter a opção de mostrar todos os imoveis salvos.
+ */
