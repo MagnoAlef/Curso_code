81d943f3bd06293a59d394de3bfacb6c3be63f36
Magno <magno.alefb@gmail.com>
2023-05-12 22:52:08 -0300

    teste com parametros em funcoes js

--- RevisaoLogicaJs/Funcao/funcao.js
+++ RevisaoLogicaJs/Funcao/funcao.js
@@ -2,3 +2,16 @@ function ola(){
     console.log("Hello,Word!!!")
 }
 ola()
+
+function teste(teste){
+   teste.idade = 30
+   teste.teste = 'Magno'
+}
+
+
+algo = {teste: 'algo' , idade: 26}
+console.log(algo)
+teste(algo)
+
+console.log(algo)
+
