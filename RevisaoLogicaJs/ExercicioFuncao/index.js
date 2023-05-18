1b8014e06fbee9579d066dc763be705933a772f3
Magno <magno.alefb@gmail.com>
2023-05-17 22:09:57 -0300

    exercicio funcao

---
+++ RevisaoLogicaJs/ExercicioFuncao/index.js
@@ -0,0 +1,13 @@
+/**
+ * Escreva um programa em js para calcular a area de diferentes 
+ * formas geometrica.O programa deve iniciar com um menu contendo as 
+ * diferentes opções de calcula.As opções devem ser.
+ * Area do triangulo -> base * altura /2
+ * Area do retangulo -> base * altura
+ * Area do quadrado -> lado * lado
+ * Area do trapezio -> (base maior + base menor) * altura /2
+ * Area do circulo -> pi * raio * raio (considere pi = 3.14)
+ * Voce deve escrever o programa usando funcoes sempre que possivel 
+ * para separar os procedimentos.O programa tambem deve ter um opção de sair 
+ * e enquanto ele nao for escolhida devera voltar ao menu.
+ *  */
