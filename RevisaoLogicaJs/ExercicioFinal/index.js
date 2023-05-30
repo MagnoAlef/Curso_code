0da06363a7837b8b5148c88c4970bd06400a5d9a
Magno <magno.alefb@gmail.com>
2023-05-29 22:26:19 -0300

    ultimo exercicio

---
+++ RevisaoLogicaJs/ExercicioFinal/index.js
@@ -0,0 +1,21 @@
+/**
+ * Escreva um programa em js que simule um sistema de vagas de emprego , onde e possivel gerenciar as 
+ * vagas e adicionar candidatos as vagas . Ele deve atender aos seguintes requisitos.
+ * -> Ter um menu onde e possivel escolher entre as diferentes funcionalidades do sistema
+ * -> Listar vagas disponiveis 
+ * -> Visualizar uma vaga
+ * -> Inscrever um cancidato em uma vaga
+ * -> Excluir uma vaga
+ * -> Sair
+ * A opção de listar as vagas deve mostrar o indice , o nome e a quantidade de candidatos inscritos de todas as vagas.
+ * A opção de criar uma nova vaga deve pedir um nome para a vaga ,uma descrição e uma data limite , e tambem 
+ * deve pedir que o usuario confirme as informações antes de salva-las.
+ * A opção de visualizar uma vaga deve pedir o  indice da vaga e mostrar todas as informações dela:indice,
+ * nome,descrição,data limite , quantidade de candidatos e o nome dos candidatos.
+ * A opção de inscrever um candidato em uma vaga deve pedir o nome do candidato, o indice da vaga e entao uma
+ * confirmação exibindo as informações da vaga antes de salvar o candidato na vaga.
+ * A opção de excluir uma vaga deve pedir o indice da vaga, mostrar suas informações e pedir que o usuario confirme
+ * a exclusão da vaga antes de realmente exclui-la.
+ * Este é o exercicio de revisão do modulo , entaõ aproveite para utilizar todos os recursos vistos ate agora sempre que 
+ * possivel, como os objwtos , arrays e funcões.
+ */
