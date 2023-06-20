c72226e86917e78984f1caa1d82d44accb5a3064
Magno <magno.alefb@gmail.com>
2023-06-19 21:18:23 -0300

    eventos js

---
+++ RevisandoJsDom/Eventos/eventos.js
@@ -0,0 +1,19 @@
+function mouseEnterEvent() {
+    console.log('Entrada do mouse')
+  }
+  
+  function changeEvent() {
+    console.log('Mudança no valor')
+  }
+  
+  function focusEvent() {
+    console.log('foco no elemento')
+  }
+  
+  function blurEvent() {
+    console.log('desfocando o elemento')
+  }
+  
+  function clickEvent() {
+    alert('clicando no elemento')
+  }
