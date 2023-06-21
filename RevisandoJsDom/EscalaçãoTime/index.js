989c98897d1db8342b6809995f1d69b11479a90d
Magno <magno.alefb@gmail.com>
2023-06-20 22:06:44 -0300

    Primeiro exercicio

--- RevisandoJsDom/EscalaçãoTime/index.js
+++ RevisandoJsDom/EscalaçãoTime/index.js
@@ -1,3 +1,17 @@
 /**
- * 
+ * Construa uma pagina web que permita monta a escalação de um time de jogadores.
+ * Nele devera ser possivel:
+ * -> Escalar um jogador
+ * -> Informar a posição do jogador
+ * -> Informar o nome do jogador
+ * -> Informar o numero da camisa
+ * Ter um botao de escalar que pergunta ao usuario se ele deseja confirmar a escalação
+ * daquele jogador e então insere as informações em uma lista da pagina
+ * Apos o jogador ser escalado os campos de texto devem ser limpos .
+ * Remover um jogador 
+ * -> Informar o numero da camisa do jogador 
+ * -> Ter um botao de remover que pergunta ao usuario se ele deseja confirmar a remoção daquele jogados
+ * e entao exclui ele da lista na pagina
+ * Apos o jogador ser removido o campo de texto dever ser limpo
+ * Dica:Lemnrando que e possivel acessar o texto de um input atraves da propriedade value
  */
