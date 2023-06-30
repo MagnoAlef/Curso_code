080099935a0316373ad483f3df2d3435d45d3598
Magno <magno.alefb@gmail.com>
2023-06-29 21:22:19 -0300

    estrutura do exercicio

---
+++ RevisandoJsDom/CadastroDevs/index.js
@@ -0,0 +1,15 @@
+/**
+ * Construa uma pagina web que permita cadastrar desenvolvedores suas tecnologias e tempos de experiencia
+ * -> A pagina deve ter um input para o nome completo do 
+ * desenvolvedor e um botao para adicionar uma nova tecnologia.
+ * -> Ao clicar no botao um evento deve ser disparado criando no 
+ * formulario uma nova linha de inputs contendo.
+ * -> um campo de texto para  o nome da tecnologia.
+ * -> um grupo de botoes tipo radio com as opções de tempo de 
+ * experiencia ( por exemplo '0/2' '3/4 '5 ou mais)
+ * -> um botão de remover que deve excluir essa linha.
+ * -> O formulario tambem dever ter um botão de cadastrar que salva as informações do dev em um array
+ * e limpa o formulario. para testar o funcionamento voce pode utilizar um simples console.log.
+ * Para esse exercicio voce deve utilizar apenas eventos adicionais via js 
+ * e a pagina deve funcionar sem acionar um recarregamento.
+ */
