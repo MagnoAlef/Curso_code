3ea6c2a48538276c9ebe37a043b4a9a7dec1bf63
Magno <magno.alefb@gmail.com>
2023-07-06 21:03:22 -0300

    estudando exercicios

--- RevisandoJsDom/CadastroDevs/index.js
+++ RevisandoJsDom/CadastroDevs/index.js
@@ -12,89 +12,45 @@
  * e limpa o formulario. para testar o funcionamento voce pode utilizar um simples console.log.
  * Para esse exercicio voce deve utilizar apenas eventos adicionais via js 
  * e a pagina deve funcionar sem acionar um recarregamento.
- */
-
+*/
 const addTechBtn = document.getElementById('addTechBtn')
-const form = document.getElementById('devForm')
-const developers = []
-let inputRows = 0
+let inputRow = 0
 
-function createLabel(text, htmlFor) {
-    const label = document.createElement('label')
-    label.htmlFor = htmlFor
-    label.innerText = text
-    return label
-  }
-  function createInput(id, value, name, type = 'text', placeholder = '') {
-    const input = document.createElement('input')
-    input.id = id
-    input.value = value
-    input.name = name
-    input.type = type
-    input.placeholder = placeholder
-    return input
-  }
+function createLabel(text,htmlFor){
+    const createLabel = document.createElement('label')
+    createLabel.innerText = text
+    createLabel.htmlFor = htmlFor
+    return createLabel
+}
 
-  addTechBtn.addEventListener('click', function (ev) {
+function createInput(id,name,value,type='text',placeholder=''){
+    const createInput = document.createElement('input')
+    createInput.id = id
+    createInput.name = name
+    createInput.value = value
+    createInput.type = type
+    createInput.placeholder = placeholder
+    return createInput
+}
+
+
+
+addTechBtn.addEventListener('click',function(){
     const stackInputs = document.getElementById('stackInputs')
-  
-    const newRow = document.createElement('li')
-    const rowIndex = inputRows
-    inputRows++
-    newRow.id = 'inputRow-' + rowIndex
-    newRow.className = 'inputRow'
-  
-    const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
-    const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
-  
-    const expLabel = createLabel('Experiência: ')
-    const id1 = 'expRadio-' + rowIndex + '.1'
-    const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel1 = createLabel('0-2 anos', id1)
-    const id2 = 'expRadio-' + rowIndex + '.2'
-    const expRadio2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel2 = createLabel('3-4 anos', id2)
-    const id3 = 'expRadio-' + rowIndex + '.3'
-    const expRadio3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
-    const expLabel3 = createLabel('5+ anos', id3)
-  
-    const removeRowBtn = document.createElement('button')
-    removeRowBtn.type = 'button'
-    removeRowBtn.innerText = 'Remover'
-    removeRowBtn.addEventListener('click', function () {
-      stackInputs.removeChild(newRow)
-    })
-  
-  
-    newRow.append(
-      techNameLabel, techNameInput, expLabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3, removeRowBtn
-    )
-  
-    stackInputs.appendChild(newRow)
-  })
 
-  form.addEventListener('submit', function (ev) {
-    ev.preventDefault()
-  
-    const fullnameInput = document.getElementById('fullname')
-    const inputRows = document.querySelectorAll('.inputRow')
-  
-    let technologies = []
-    inputRows.forEach(function (row) {
-      // #rowId input[name="techName"]
-      const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value
-      const techExp = document.querySelector('#' + row.id + ' input[type="radio"]:checked').value
-      technologies.push({ name: techName, exp: techExp })
-    })
-  
-    const newDev = { fullname: fullnameInput.value, technologies: technologies }
-    developers.push(newDev)
-    alert('Dev cadastrado com sucesso!')
-  
-    fullnameInput.value = ''
-    inputRows.forEach(function (row) {
-      row.remove()
-    })
-  
-    console.log(developers)
-  })
+    const newRow = document.createElement('li') // criar li
+    let numberRow = inputRow 
+    numberRow++
+    newRow.id = 'inputRow'+numberRow // adicionar id diferente a li
+    newRow.className = 'inputRow'
+     
+    const techNameLabel = createLabel('Nome:','techName'+numberRow ) // criar label 
+    const techNameInput = createInput('techName-'+numberRow,'techName' , null) //criar input
+
+    newRow.append(techNameLabel,techNameInput) // adicionar label e input na li
+
+    stackInputs.append(newRow) //adicionar li dentro da ul
+
+
+
+})
