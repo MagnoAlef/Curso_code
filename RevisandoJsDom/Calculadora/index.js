b2c005e0f82d2d108f5a7ca026334425e033c2cd
Magno <magno.alefb@gmail.com>
2023-07-18 20:39:29 -0300

    projeto calculadora

--- RevisandoJsDom/Calculadora/index.js
+++ RevisandoJsDom/Calculadora/index.js
@@ -1,100 +1,81 @@
-const main = document.querySelector("main");
-const root = document.querySelector(":root");
-const input = document.getElementById("input");
-const resultInput = document.getElementById("result");
-
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
 // desabilitar caracteres inexistentes
-const allowedKeys = [
-  "(",
-  ")",
-  "/",
-  "*",
-  "-",
-  "+",
-  "9",
-  "8",
-  "7",
-  "6",
-  "5",
-  "4",
-  "3",
-  "2",
-  "1",
-  "0",
-  ".",
-  "%",
-  " ",
-];
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+
 
 //Funcionalidades dos botoes
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
-  charKeyBtn.addEventListener("click", function () {
-    const value = charKeyBtn.dataset.value;
-    input.value += value;
-  });
-});
+    charKeyBtn.addEventListener("click", function () {
+      const value = charKeyBtn.dataset.value
+      input.value += value
+    })
+  })
 
 //Limpar calculadora
 document.getElementById("clear").addEventListener("click", function () {
-  input.value = "";
-  input.focus();
-});
+    input.value = ""
+    input.focus()
+  })
 
 //Botao =
 document.getElementById("equal").addEventListener("click", calculate); // Pegando o botao =  e associando a funcao de calcular para ele
 
 input.addEventListener("keydown", function (ev) {
-  ev.preventDefault();
-  if (allowedKeys.includes(ev.key)) {
-    // ev.key teclas
-    input.value += ev.key;
-    return;
-  }
-  if (ev.key === "Backspace") {
-    input.value = input.value.slice(0, -1);
-  }
-  if (ev.key === "Enter") {
-    calculate();
-  }
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)) {
+      input.value += ev.key
+      return
+    }
+    if (ev.key === "Backspace") {
+      input.value = input.value.slice(0, -1)
+    }
+    if (ev.key === "Enter") {
+      calculate()
+    }
+  })
 
-  function calculate() {
-    resultInput.value = "Error"
-    resultInput.classList.add('error')
-    const resul = eval(input.value); //eval avaliar o codigo e executa
-    resultInput.value = resul;
-    resultInput.classList.remove('error')
-    //console.log('Calculado')
-  }
-}); // heydown quando a tecla foi apertada
+document.getElementById("equal").addEventListener("click", calculate)
+
+function calculate() {
+  resultInput.value = "ERROR"
+  resultInput.classList.add("error")
+  const result = eval(input.value)
+  resultInput.value = result
+  resultInput.classList.remove("error")
+}; // heydown quando a tecla foi apertada
 
 
 //Troca theme
 
-document.getElementById('themeSwitcher').addEventListener('click',function(){
-    if(main.dataset.theme === 'dark'){
-        root.style.setProperty('--bg-color','#f1f5f9')
-        root.style.setProperty('--border-color','#aaa')
-        root.style.setProperty('--font-color','#212529')
-        root.style.setProperty('--primary-color','#26834a')
-        main.dataset.theme = 'ligth'
-    }else{
-        root.style.setProperty('--bg-color','#212529')
-        root.style.setProperty('--border-color','#aaa')
-        root.style.setProperty('--font-color','#f1f5f9')
-        root.style.setProperty('--primary-color','#4dff91')
-        main.dataset.theme = 'dark'
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+    if (main.dataset.theme === "dark") {
+      root.style.setProperty("--bg-color", "#f1f5f9")
+      root.style.setProperty("--border-color", "#aaa")
+      root.style.setProperty("--font-color", "#212529")
+      root.style.setProperty("--primary-color", "#26834a")
+      main.dataset.theme = "light"
+    } else {
+      root.style.setProperty("--bg-color", "#212529")
+      root.style.setProperty("--border-color", "#665")
+      root.style.setProperty("--font-color", "#f1f5f9")
+      root.style.setProperty("--primary-color", "#4dff91")
+      main.dataset.theme = "dark"
     }
-})
+  })
 
 //Copia para a area de transferencia
-document.getElementById('copyToClipboard').addEventListener('click',function(ev){
+document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
     const button = ev.currentTarget
-    if(button.innerText === 'Copy'){
-        button.innerText = 'Copied!'
-        button.classList.add('success')
-        navigator.clipboard.writeText(resultInput)
-    }else{
-        button.innerText = 'Copy'
-        button.classList.remove('success')
+    if (button.innerText === "Copy") {
+      button.innerText = "Copied!"
+      button.classList.add("success")
+      navigator.clipboard.writeText(resultInput.value)
+    } else {
+      button.innerText = "Copy"
+      button.classList.remove("success")
     }
-})
+  })
