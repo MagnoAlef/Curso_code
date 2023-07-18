3baed027c198f3bb72aed06956cedee859190006
Magno <magno.alefb@gmail.com>
2023-07-18 20:40:03 -0300

    projeto calculadora1

--- RevisandoJsDom/Calculadora/Professor.js
+++ RevisandoJsDom/Calculadora/Professor.js
@@ -61,7 +61,7 @@ document.getElementById("themeSwitcher").addEventListener("click", function () {
     main.dataset.theme = "light"
   } else {
     root.style.setProperty("--bg-color", "#212529")
-    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--border-color", "#665")
     root.style.setProperty("--font-color", "#f1f5f9")
     root.style.setProperty("--primary-color", "#4dff91")
     main.dataset.theme = "dark"
