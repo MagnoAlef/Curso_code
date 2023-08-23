2124d82a14c3a26857476a2069285eaf243015db
Magno <magno.alefb@gmail.com>
2023-08-23 08:42:44 -0300

    Exercicio modularizacao

---
+++ JsModerno/Modulos/ExercicioModularizacao/index.js
@@ -0,0 +1,9 @@
+/**
+ * Para esse exercicio voce ira utilizar o codigo do projeto Calc.js, desenvolvido
+ * nas aulas Projeto Pratico Guiado do modulo anterior , Js||-Dom.
+ * Voce devera modularizar o codigo js da aplicação, dividindo o conteudo do
+ * arquivo index.js em diferentes modulos utilizando a sintaxe dos ES Modules.
+ * Seu projeto deve conter pelo menos 3 modulos.
+ * Caso você não tenha os arquivos do projeto Calc.js
+ * você podera baixa-lo na aba de materias dessa aula. 
+ */
