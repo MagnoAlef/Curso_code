2772e1de1a421e0dd849ae38ec4ad538bbc1b2c8
Magno <magno.alefb@gmail.com>
2023-08-30 08:30:37 -0300

    reduce e map

--- JsModerno/teste.js
+++ JsModerno/teste.js
@@ -1,8 +1,11 @@
 
 const itens = [1,2,3,4,5,56,6,7,8]
- const teste = itens.reduce((acumulador , x) => acumulador + x , 0)
+ const reduce = itens.reduce((acumulador , x) => acumulador + x , 0)
+const map = itens.map((x) => x)
+console.log(reduce)
+console.log(map)
 
- console.log(teste)
+
 
 
 
