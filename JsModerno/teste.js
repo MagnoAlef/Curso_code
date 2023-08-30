23a81189433d84be2d6634ad0e894b7aac96c26a
Magno <magno.alefb@gmail.com>
2023-08-29 22:48:03 -0300

    foreach

--- JsModerno/teste.js
+++ JsModerno/teste.js
@@ -1,22 +1,13 @@
-// Função para gerar um objeto de pessoa aleatoriamente
-function criarPessoa() {
-    const nomes = ["Alice", "Bob", "Carol", "David", "Eve"];
-    const idades = [25, 30, 22, 28, 35];
-    const cidades = ["São Paulo", "Rio de Janeiro", "Belo Horizonte", "Curitiba", "Salvador"];
-  
-    const nomeAleatorio = nomes[Math.floor(Math.random() * nomes.length)];
-    const idadeAleatoria = idades[Math.floor(Math.random() * idades.length)];
-    const cidadeAleatoria = cidades[Math.floor(Math.random() * cidades.length)];
-  
-    return { nome: nomeAleatorio, idade: idadeAleatoria, cidade: cidadeAleatoria };
-  }
-  
-  // Gerar um array de 5 objetos de pessoas
-  const pessoas = Array.from({ length: 5 }, criarPessoa);
-  
-  console.log(pessoas);
-  console.log(pessoas.map((x) => x.idade))
-  console.log(pessoas.map((x) => x.nome.toUpperCase))
-  console.log(pessoas.map((x) => `${x.nome}-${x.idade}` ))
+data = {
+  data : [
+    { id : 1 , label : 'juliana'},
+    { id : 2 , label : 'magno'},
+    { id : 3 , label : 'juliana'},
+    { id : 4 , label : 'juliana'},
+    { id : 5 , label : 'juliana'},
+    { id : 6 , label : 'juliana'},
+  ]
+}
 
-   pessoas.forEach((x) => console.log('nome e idade das pessoas' , `${x.nome} tem ${x.idade}`))
+const itens = data.data.forEach((x) => console.log(x))
+console.log(itens)
