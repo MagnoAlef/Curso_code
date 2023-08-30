aac7dd5ab1180dc783f1d9316ae18e3efb28c61c
Magno <magno.alefb@gmail.com>
2023-08-29 22:50:35 -0300

    foreach teste

--- JsModerno/teste.js
+++ JsModerno/teste.js
@@ -1,13 +1,25 @@
-data = {
-  data : [
-    { id : 1 , label : 'juliana'},
-    { id : 2 , label : 'magno'},
-    { id : 3 , label : 'juliana'},
-    { id : 4 , label : 'juliana'},
-    { id : 5 , label : 'juliana'},
-    { id : 6 , label : 'juliana'},
-  ]
-}
+const data = {
+  data: [
+    { id: 1, label: 'juliana' },
+    { id: 2, label: 'magno' },
+    { id: 3, label: 'juliana' },
+    { id: 4, label: 'juliana' },
+    { id: 5, label: 'juliana' },
+    { id: 6, label: 'juliana' },
+  ],
+};
 
-const itens = data.data.forEach((x) => console.log(x))
-console.log(itens)
+const itens = [];
+data.data.forEach((x) => {
+  itens.push({
+    itens: x,
+  });
+});
+
+console.log(itens);
+
+
+
+
+
+
