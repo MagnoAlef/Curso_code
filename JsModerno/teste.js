5e35797dd4176c9ab980f29c6f469695505c4788
Magno <magno.alefb@gmail.com>
2023-08-31 08:43:07 -0300

    estudando metodos do js

--- JsModerno/teste.js
+++ JsModerno/teste.js
@@ -5,6 +5,17 @@ const map = itens.map((x) => x)
 console.log(reduce)
 console.log(map)
 
+const testes = [
+    {id:1 , nome: 'magno',valor:20},
+    {id:1 , nome: 'marcos',valor: 30},
+    {id:1 , nome: 'matheus',valor: 40},
+    {id:1 , nome: 'maria',valor: 50},
+    {id:1 , nome: 'maraisa',valor: 60},
+    {id:1 , nome: 'maiara',valor: 70},
+    {id:1 , nome: 'maira',valor: 80},
+    ]
+console.log(testes.find((x) => x.nome === 'magno'))
+
 
 
 
