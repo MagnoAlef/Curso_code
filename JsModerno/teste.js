b0e3e2f37226cc1c4a022b00adb6375610f71ff0
Magno <magno.alefb@gmail.com>
2023-09-01 08:42:04 -0300

    filter

--- JsModerno/teste.js
+++ JsModerno/teste.js
@@ -10,12 +10,14 @@ const testes = [
     {id:1 , nome: 'marcos',valor: 30},
     {id:1 , nome: 'matheus',valor: 40},
     {id:1 , nome: 'maria',valor: 50},
-    {id:1 , nome: 'maraisa',valor: 60},
+    {id:1 , nome: 'maiara',valor: 60},
     {id:1 , nome: 'maiara',valor: 70},
     {id:1 , nome: 'maira',valor: 80},
     ]
 
     console.log(testes.map((x) => x.valor).reduce((acumulador , x) => acumulador + x , 0))
+
+    console.log(testes.filter((x) =>  x.nome === 'maiara' && x.valor === 70 ))
 
 
 
