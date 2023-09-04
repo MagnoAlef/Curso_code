44450fdc00957265e45699c4cd023f6121418d6b
Magno <magno.alefb@gmail.com>
2023-09-04 08:51:39 -0300

    usando pacotes npm

---
+++ JsModerno/UsandoNpm/index.js
@@ -0,0 +1,4 @@
+const lodash = require ('lodash')
+
+console.log(lodash.isArray([]))
+console.log(lodash.kebabCase('Magno Alef Barroso')) // Forma de escrever separando por tracos - Magno-Alef-Barroso
