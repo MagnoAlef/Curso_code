2438a7b8af0000daf27ce14e66e3924b92043d1c
Magno <magno.alefb@gmail.com>
2023-09-19 08:40:10 -0300

    exercicio user finalizado

--- JsPoo/CriandoClasse/User.js
+++ JsPoo/CriandoClasse/User.js
@@ -7,3 +7,27 @@
  * devera comparar esses parametros com o email e a senha do usuario e mostrar 
  * uma mensagem no console dizendo se o login foi bem sucedido ou não
  */
+
+class User {
+    constructor(fullname , email , password){
+        this.fullname = fullname
+        this.email = email
+        this.password = password
+
+    }
+    login(email,senha){
+        if(this.email === email && this.password === senha ){
+            console.log(`${this.email} e ${this.senha} São iguais a ${email} e ${senha}`)
+        }else{
+            console.log('Email e senha não correspondem.')
+        }
+
+    }
+}
+
+const usuario = new User('Magno', 'Magno.alefb@gmail.com' , '12345')
+console.log(usuario)
+
+console.log(usuario.login('Magno.alefb@gmail.com','12345'))
+
+console.log(usuario.login('Magno.alefb@gmail.','12345'))
