c4c434bc1d777ecb499a138a1b3233e9f2396c33
Magno <magno.alefb@gmail.com>
2023-09-19 08:24:46 -0300

    exercicio classes

---
+++ JsPoo/CriandoClasse/Product.js
@@ -0,0 +1,11 @@
+/**
+ * Product, que devera possui:
+ * .um atributo name, atribuivel na instanciação
+ * .um atributo description , atribuivel na instanciação
+ * .um atributo price, atribuivel na instancição
+ * .um atributo inStock , iniciado sempre com 0
+ * .um metodo addToStock, que tem como parametro a quantidade a ser adicionada
+ * em estoque e deve somar essa quantidade a variavel inStock
+ * .um metodo calculateDiscount, que tem como parametro a percentagem de 
+ * desconto a ser aplicada e retorne o valor do preco com o desconto aplicado
+ */
---
+++ JsPoo/CriandoClasse/User.js
@@ -0,0 +1,9 @@
+/**
+ * User, que devera possuir:
+ * .um atributo fullname, atribuivel na instanciação
+ * .um atributo email, atribuivel na instanciação
+ * .um atributo password atribuivel ma instanciação
+ * .um metodo login, que tem como parametro um email e senha e 
+ * devera comparar esses parametros com o email e a senha do usuario e mostrar 
+ * uma mensagem no console dizendo se o login foi bem sucedido ou não
+ */
