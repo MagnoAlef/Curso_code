a557eaa447cff3f787d63c083dd34cc754933f92
Magno <magno.alefb@gmail.com>
2023-10-09 08:52:55 -0300

    exercicio Personagem correto finalizado 1

--- JsPoo/ExercicioPersonagemJogo/Teste.js
+++ JsPoo/ExercicioPersonagemJogo/Teste.js
@@ -4,7 +4,7 @@ const Warrior = require("./Warrior");
 
 const arthur = new Mage('Arthur', 90, 4, 2, 14)
 const beatrice = new Thief('Beatrice', 140, 22, 8)
-const caine = new Warrior('Cain', 200, 14, 12, 4)
+const caine = new Warrior('Caine', 200, 14, 12, 4)
 
 console.table({ arthur, beatrice, caine })
 
