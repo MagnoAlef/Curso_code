14da46b2876ac06e8718a2a91c5de8c53ca63370
Magno <magno.alefb@gmail.com>
2023-10-09 08:52:27 -0300

    exercicio Personagem correto finalizado

--- JsPoo/ExercicioPersonagemJogo/Teste.js
+++ JsPoo/ExercicioPersonagemJogo/Teste.js
@@ -4,18 +4,18 @@ const Warrior = require("./Warrior");
 
 const arthur = new Mage('Arthur', 90, 4, 2, 14)
 const beatrice = new Thief('Beatrice', 140, 22, 8)
-const cain = new Warrior('Cain', 200, 14, 12, 4)
+const caine = new Warrior('Cain', 200, 14, 12, 4)
 
-console.table({ arthur, beatrice, cain })
+console.table({ arthur, beatrice, caine })
 
-cain.switchStance()
-arthur.attack(cain)
+caine.switchStance()
+arthur.attack(caine)
 beatrice.attack(arthur)
 
-console.table({ arthur, beatrice, cain })
+console.table({ arthur, beatrice, caine })
 
-cain.attack(arthur)
+caine.attack(arthur)
 arthur.heal(arthur)
-beatrice.attack(cain)
+beatrice.attack(caine)
 
-console.table({ arthur, beatrice, cain })
+console.table({ arthur, beatrice, caine })
