17fb4dec2532495934aeb7bb5e6ae898cb624678
Magno <magno.alefb@gmail.com>
2024-01-25 16:03:51 -0300

    teste

---
+++ JsDom/teste.js
@@ -0,0 +1,625 @@
+const teste = [
+    {
+        "id": 20724,
+        "nome": "ABCDe",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 21442,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 32209,
+        "nome": "agrohubteste@blondmail.com",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 36326,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 32965,
+        "nome": "Agropecuária 3 irmaos",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 37224,
+                "nome": "Fornecedor"
+            },
+            {
+                "id": 5,
+                "pessoaFazendaId": 37223,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 11857,
+        "nome": "ajuda AgroHUB",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 8255,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 30716,
+        "nome": "Aparecido Alves dos Santos",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 34686,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 12938,
+        "nome": "Bárbara Aparecida Andrade",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 9295,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 13274,
+        "nome": "Campo Nutrição Animal ",
+        "celular": "",
+        "tipoPessoa": "2",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 9717,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 18948,
+        "nome": "Campo xyz",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 16352,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 18277,
+        "nome": "Cassiele Oliveira",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 15658,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 6452,
+        "nome": "Cassio Rodrigues",
+        "celular": "(31) 99239-0672",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 29343,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 29395,
+        "nome": "Comigo",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 33119,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 10901,
+        "nome": "Cooperativa",
+        "celular": "(49) 99999-9999",
+        "tipoPessoa": "2",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 13504,
+                "nome": "Cliente"
+            },
+            {
+                "id": 2,
+                "pessoaFazendaId": 7584,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 25034,
+        "nome": "Fornecedor adubos",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 28474,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 32780,
+        "nome": "Fornecedor de Diesel",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 37015,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 10893,
+        "nome": "Fornecedor de Garrotes",
+        "celular": "",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 7580,
+                "nome": "Fornecedor"
+            },
+            {
+                "id": 4,
+                "pessoaFazendaId": 10527,
+                "nome": "Terceirizado"
+            }
+        ]
+    },
+    {
+        "id": 13377,
+        "nome": "Fornecedor de Ração",
+        "tipoPessoa": "2",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 9857,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 25035,
+        "nome": "Fornecedor Herbicida",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 28475,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 10894,
+        "nome": "Frigorífico",
+        "celular": "",
+        "tipoPessoa": "2",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 7581,
+                "nome": "Cliente"
+            },
+            {
+                "id": 2,
+                "pessoaFazendaId": 15666,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 13298,
+        "nome": "Gesta'up Pecuária com Lucro ",
+        "celular": "",
+        "tipoPessoa": "2",
+        "papeisPessoa": [
+            {
+                "id": 6,
+                "pessoaFazendaId": 9755,
+                "nome": "Sócio"
+            }
+        ]
+    },
+    {
+        "id": 23228,
+        "nome": "Guilherme Fonseca",
+        "celular": "(62) 99939-4191",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 32771,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 13249,
+        "nome": "joão",
+        "celular": "(65) 99963-2581",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 6,
+                "pessoaFazendaId": 9671,
+                "nome": "Sócio"
+            }
+        ]
+    },
+    {
+        "id": 10935,
+        "nome": "João (vizinho)",
+        "celular": "",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 7592,
+                "nome": "Cliente"
+            }
+        ]
+    },
+    {
+        "id": 14135,
+        "nome": "José",
+        "celular": "",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 10608,
+                "nome": "Cliente"
+            }
+        ]
+    },
+    {
+        "id": 14148,
+        "nome": "josé maria",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 10623,
+                "nome": "Cliente"
+            }
+        ]
+    },
+    {
+        "id": 14242,
+        "nome": "José ricardo",
+        "celular": "",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 10735,
+                "nome": "Cliente"
+            }
+        ]
+    },
+    {
+        "id": 25935,
+        "nome": "Juliana Aparecida Gomes de Oliveira",
+        "celular": "(31) 98289-6223",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 29478,
+                "nome": "Fornecedor"
+            },
+            {
+                "id": 3,
+                "pessoaFazendaId": 29479,
+                "nome": "Funcionário"
+            }
+        ]
+    },
+    {
+        "id": 18949,
+        "nome": "Juliana Aparecida Gomes de Oliveira Barroso",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 3,
+                "pessoaFazendaId": 33087,
+                "nome": "Funcionário"
+            }
+        ]
+    },
+    {
+        "id": 20007,
+        "nome": "Juliana Aparecida Gomes de Oliveira Barroso",
+        "celular": "(31) 98289-6223",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 34286,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 25936,
+        "nome": "Juliana Aparecida Gomes de Oliveira2",
+        "celular": "(31) 98289-6223",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 29480,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 23827,
+        "nome": "Kika Bueno ",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 27346,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 11798,
+        "nome": "lG INfomatica ",
+        "celular": "",
+        "tipoPessoa": "2",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 8130,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 23353,
+        "nome": "Loja de Produtos Agrícolas",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 2,
+                "pessoaFazendaId": 26660,
+                "nome": "Fornecedor"
+            }
+        ]
+    },
+    {
+        "id": 12936,
+        "nome": "Luiz Felipe Mendes Rocha",
+        "celular": "(33) 98838-5768",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 9297,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 29394,
+        "nome": "Marcelo",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 3,
+                "pessoaFazendaId": 33118,
+                "nome": "Funcionário"
+            }
+        ]
+    },
+    {
+        "id": 6177,
+        "nome": "Maria Erika Picharillo",
+        "celular": "(16) 99778-0523",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 32243,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 32958,
+        "nome": "Maria Tereza Grasel",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 37217,
+                "nome": "Cliente"
+            },
+            {
+                "id": 3,
+                "pessoaFazendaId": 37219,
+                "nome": "Funcionário"
+            },
+            {
+                "id": 5,
+                "pessoaFazendaId": 37218,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 6925,
+        "nome": "Maria Tereza Grasel Rodrigues",
+        "celular": "(31) 99815-9714",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 7915,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 30058,
+        "nome": "MARISA ALVES PEREIRA",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 34627,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 13299,
+        "nome": "Sebastião",
+        "celular": "(11) 98411-5698",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 3,
+                "pessoaFazendaId": 9756,
+                "nome": "Funcionário"
+            }
+        ]
+    },
+    {
+        "id": 13356,
+        "nome": "Sebastião ",
+        "celular": "",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 6,
+                "pessoaFazendaId": 9820,
+                "nome": "Sócio"
+            }
+        ]
+    },
+    {
+        "id": 18316,
+        "nome": "Simone Garcia",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 15669,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 18570,
+        "nome": "Teste",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 7,
+                "pessoaFazendaId": 15954,
+                "nome": "Outros"
+            }
+        ]
+    },
+    {
+        "id": 14751,
+        "nome": "Teste 2",
+        "celular": "(66) 99988-1721",
+        "papeisPessoa": [
+            {
+                "id": 1,
+                "pessoaFazendaId": 11409,
+                "nome": "Cliente"
+            }
+        ]
+    },
+    {
+        "id": 7736,
+        "nome": "TI AgroHUB",
+        "celular": "(31) 99998-9999",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 5,
+                "pessoaFazendaId": 36367,
+                "nome": "Usuário"
+            }
+        ]
+    },
+    {
+        "id": 10903,
+        "nome": "Vaqueiro",
+        "celular": "(49) 99999-9999",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 3,
+                "pessoaFazendaId": 7586,
+                "nome": "Funcionário"
+            }
+        ]
+    },
+    {
+        "id": 10934,
+        "nome": "Veterinário",
+        "celular": "",
+        "papeisPessoa": [
+            {
+                "id": 4,
+                "pessoaFazendaId": 7591,
+                "nome": "Terceirizado"
+            }
+        ]
+    },
+    {
+        "id": 11797,
+        "nome": "Zé dirceu",
+        "celular": "(67) 99963-0607",
+        "tipoPessoa": "1",
+        "papeisPessoa": [
+            {
+                "id": 3,
+                "pessoaFazendaId": 8129,
+                "nome": "Funcionário"
+            }
+        ]
+    }
+]
+// some pelo menos um elemento do array satistaz a funcao
+const fornecedores =teste.filter((x) => x.papeisPessoa.some((x) => x.nome === 'Terceirizado')).map((x) => ({
+    value: x.id,
+    nome:x.nome
+}))
+console.log('forncedr' , fornecedores)
